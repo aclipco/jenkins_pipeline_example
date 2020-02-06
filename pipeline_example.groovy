291afd322a8ee5d6b0f53a73c663813b185b791a
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-02-06 12:54:40 -0600

    pipeline added

---
+++ pipeline_example.groovy
@@ -0,0 +1,56 @@
+node {
+    properties([
+        buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
+        pipelineTriggers([cron('* * * * *')])
+        ])
+
+    stage("Pull Repo"){
+        //This line pulls a repo
+        git 'git@github.com:aclipco/k8-objects-exercise.git'
+    }
+    stage("Deploy to Dev"){
+        echo "Hello World"
+    }
+    stage("Wait"){
+        //input 'Deploy to QA? '
+        //sleep 3
+    }
+    stage("Perform Some Other Task"){
+        //This will call another job
+        //build "Item1"
+    }
+    stage("Deploy to QA"){
+        echo "Hello World"
+    }
+    stage("Perform task on Husein Machine")
+        // Performs task on remote system
+        //sh "curl http://jenkins_master.acirrustech.com:8080/job/Item1/build?token=ITEM1"
+    stage("Script"){
+		sh label: '', script: 
+		'''#!/bin/bash
+			if [ ! -f /tmp/foo.txt ]; 
+			then
+				echo "File not found!"
+				echo "Creating a file"
+				touch "/tmp/foo.txt"
+			fi
+		'''
+	}
+    stage("Timestamp"){
+		timestamps {
+			echo "hello"
+		}
+	}
+    stage("Failure"){
+        //error 'Intentional Failure'
+    }
+
+    stage("Send Notification to slack"){
+        echo "Hello World"
+        slackSend channel: 'nagios_alerts', message: 'Completed'
+        mail bcc: 'farrukh@gmail.com', body: 'message', cc: 'farrukh@gmail.com', from: '', replyTo: '', subject: 'Completed', to: 'farrukh@gmail.com'
+    }
+    stage("Clean WS"){
+        cleanWs()
+    }
+}
