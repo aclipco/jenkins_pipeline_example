8b3a2e0c9dab91794449362872cbf33976b408b8
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-02-06 15:06:33 -0600

    email added

--- pipeline_example.groovy
+++ pipeline_example.groovy
@@ -12,5 +12,6 @@ stage("Stage3"){
 stage("notifications to slack"){
   echo "Hello Word"
   slackSend channel: 'nagios_alerts', message: 'Completed'
+  mail bcc: 'anastasiaclipco@gmail.com', body: 'message', cc: 'anastasiaclipco@gmail.com', from: '', replyTo: '', subject: 'test', to: 'anastasiaclipco@gmail.com'
   }
 }
